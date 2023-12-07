d4005e14f63ef248ed7209435e5f21fc1de2887e
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-12-07 10:59:47 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace dodge_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Balltimer = new System.Windows.Forms.Timer(this.components);
+            this.scorelable = new System.Windows.Forms.Label();
+            this.Winlable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Balltimer
+            // 
+            this.Balltimer.Enabled = true;
+            this.Balltimer.Interval = 20;
+            this.Balltimer.Tick += new System.EventHandler(this.Balltimer_Tick);
+            // 
+            // scorelable
+            // 
+            this.scorelable.BackColor = System.Drawing.Color.Transparent;
+            this.scorelable.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelable.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.scorelable.Location = new System.Drawing.Point(246, 16);
+            this.scorelable.Name = "scorelable";
+            this.scorelable.Size = new System.Drawing.Size(100, 23);
+            this.scorelable.TabIndex = 0;
+            this.scorelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Winlable
+            // 
+            this.Winlable.BackColor = System.Drawing.Color.Transparent;
+            this.Winlable.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Winlable.Location = new System.Drawing.Point(192, 149);
+            this.Winlable.Name = "Winlable";
+            this.Winlable.Size = new System.Drawing.Size(193, 69);
+            this.Winlable.TabIndex = 1;
+            this.Winlable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.Winlable);
+            this.Controls.Add(this.scorelable);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Balltimer;
+        private System.Windows.Forms.Label scorelable;
+        private System.Windows.Forms.Label Winlable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dodge_game
+{
+    public partial class Form1 : Form
+    {
+        int playerscore; 
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool dDown = false;
+        bool aDown = false;
+        bool leftarrowDown = false;
+        bool rightarrowDown = false;
+
+        Rectangle player = new Rectangle(220, 305, 20, 20);
+        int playerSpeed = 10;
+
+        List<Rectangle> balls = new List<Rectangle>();
+        int ballsize = 10;
+        int ballspeed = 8;
+
+        List<Rectangle> obstical = new List<Rectangle>();
+
+        SolidBrush lightblueBrush = new SolidBrush(Color.LightBlue);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush greenBrush = new SolidBrush(Color.LawnGreen);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush purpleBrush = new SolidBrush(Color.MediumPurple);
+
+        Random randgen = new Random();
+        int randValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftarrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightarrowDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightarrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftarrowDown = false;
+                    break;
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(lightblueBrush, player);
+            
+
+
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                e.Graphics.FillEllipse(purpleBrush, balls[i]);
+            }
+        }
+
+        private void Balltimer_Tick(object sender, EventArgs e)
+        {
+            if (wDown == true && player.Y > 0)
+            {
+                player.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player.Y < this.Height - player.Height)
+            {
+                player.Y += playerSpeed;
+            }
+
+            if (aDown == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (dDown == true && player.X < this.Width - player.Width)
+            {
+                player.X += playerSpeed;
+            }
+
+            if (upArrowDown == true && player.Y > 0)
+            {
+                player.Y -= playerSpeed;
+            }
+            if (downArrowDown == true && player.Y < this.Height - player.Height)
+            {
+                player.Y += playerSpeed;
+            }
+
+            if (leftarrowDown == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (rightarrowDown == true && player.X < this.Width - player.Width)
+            {
+                player.X += playerSpeed;
+            }
+
+            randValue = randgen.Next(1, 11);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = randgen.Next(1,10);
+                Rectangle newball = new Rectangle(x, 0, ballsize, ballsize);
+                balls.Add(newball);
+            }
+
+            for(int i = 0; i < balls.Count(); i++)
+            {
+                if (balls[i].IntersectsWith(player))
+                {
+                    playerscore -= 5;
+                    balls.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < obstical.Count(); i++)
+            {
+                if (obstical[i].IntersectsWith(player))
+                {
+                    playerscore = 0;
+          
+                }
+            }
+
+            if (playerscore < 0 )
+            {
+                Winlable.ForeColor = Color.Red;
+                Winlable.Text = $" You Lose ";
+            }
+
+            
+        }
+    }
+}
